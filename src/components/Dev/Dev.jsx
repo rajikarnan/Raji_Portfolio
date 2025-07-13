91abacd5e8c46a3c6339266a2d7e6b8786ef2c21
rajikarnan <rajikarnan24@gmail.com>
2025-07-14 10:15:43 +1200

    Summary update

--- src/components/Dev/Dev.jsx
+++ src/components/Dev/Dev.jsx
@@ -15,13 +15,10 @@ export const Dev = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Raji Karnan</h1>
         <p className={styles.description}>
-          Experienced Full-Stack Developer based in Napier, New Zealand, with a
-          robust 5-year background in developing innovative technical solutions
-          across various industries. Proven expertise in leading projects and
-          teams to success, specializing in ReactJS, NodeJS, and VueJS among
-          other technologies. Demonstrated ability to improve operational
-          efficiency and project delivery through effective problem-solving and
-          process optimization.
+          A passionate full‑stack developer who transforms ideas into sleek,
+          user-friendly web applications. With a strong foundation in
+          JavaScript, React, Vue and Node.js, I build meaningful digital experiences
+          that prioritize clarity, performance, and user engagement. 
         </p>
         <div className={styles.actionButtons}>
           <a
